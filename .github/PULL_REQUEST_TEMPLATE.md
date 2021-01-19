7750a5799fb8d4cad2521a0c7f5256f39f942a9d
lus21 <lus21484@gmail.com>
2021-01-19 16:15:48 +0400

    add redux and  PR template

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,31 @@
+## Story
+
+<!-- Please copy-paste here task URL from the *JIRA/Trello/Pivotal* -->
+
+[Story Link](#)
+
+## Motivation
+
+<!-- Please describe your task here, can be copy-pasted from the *JIRA/Trello/Pivotal* -->
+
+
+## Test Plan
+
+<!-- Please describe how this should be tested, steps how to use, and failure cases if they are exists -->
+
+## Pull Request Type
+
+What kind of change does this PR introduce?
+
+<!-- Please check the one that applies to this PR using "x". -->
+
+- [ ] Bugfix
+- [ ] Feature
+- [ ] Code style update (formatting, local variables)
+- [ ] Refactoring (no functional changes, no api changes)
+- [ ] Build related changes
+- [ ] CI related changes
+- [ ] Documentation content changes
+- [ ] Other... Please describe:
+
+## Other information
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 import './App.css';
+import { IRootState } from './store';
+import { DemoActions } from './store/demo/types';
+import * as actions from './store/demo/actions';
+import * as asyncactions from './store/demo/async-actions';
 
-function App() {
+const mapStateToProps = ({ demo }: IRootState) => {
+  const { list, loading } = demo;
+  return { list, loading };
+}
+
+const mapDispatcherToProps = (dispatch: Dispatch<DemoActions>) => {
+  return {
+    addItem: (item: string) => dispatch(actions.addItemToList(item)),
+    addItemAsync: (item: string) => asyncactions.addItemAsync(dispatch, item)
+  }
+}
+
+type ReduxType = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatcherToProps>;
+
+type IState = string;
+
+function App(props: ReduxType) {
+  const [inputValue, setInputValue] = useState<IState>('');
+  const { list, addItemAsync, loading } = props;
+
+  const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const value = event?.currentTarget?.value;
+    setInputValue(value);
+  }
+
+  const handleAddClick = () => {
+    addItemAsync(inputValue);
+    setInputValue('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ margin: '20px' }}>
+        <input value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleAddClick}>Add</button>
+        <ul>
+          {list.map(l => <li key={l}>{l}</li>)}
+        </ul>
+      </div>
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatcherToProps)(App);
